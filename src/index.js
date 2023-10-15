a94f421365f7732d4f1ad5476c8b78653c3493e7
Vanchenko <vanchenkoev@pakline.com.ua>
2023-10-15 21:14:58 +0300

    routes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='https://github.com/Vanchenko/tweets-card'>
+    <BrowserRouter basename='https://github.com/Vanchenko/tweets-card/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>

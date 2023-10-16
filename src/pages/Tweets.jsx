9d56584ae5106be4982b73f6bb1be99804e9bcfa
Vanchenko <vanchenkoev@pakline.com.ua>
2023-10-16 10:09:40 +0300

    Update Tweets.jsx

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -2,17 +2,14 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { TweetsCardList } from '../components/TweetsCardList/TweetsCardList';
 import { Button } from '../components/Button/Button';
-import { Wrapper, Text, Loader } from './Tweets.styled';
+import { Wrapper } from './Tweets.styled';
 import { ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import { ColorRing } from 'react-loader-spinner';
 import { loadUsers } from 'api/api';
 import { BackLink } from "../components/Backlink/Backlink";
 
 const Tweets = () => {
   const [dataTweets, setDataTweets] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const location = useLocation();
   const backLinkHref = location.state?.from ?? '/home';
@@ -41,21 +38,7 @@ const Tweets = () => {
         theme="colored"
         style={{ top: '1px' }}
       />
-      {isLoading && (
-        <Loader>
-          <ColorRing
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="blocks-loading"
-            wrapperStyle={{}}
-            wrapperClass="blocks-wrapper"
-            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-          />
-        </Loader>
-      )}
       {dataTweets.length > 0 && <TweetsCardList tweetslist={dataTweets} />}
-      {error && <Text>{error}</Text>}
       <Button onLoadMore={onLoadMore} />
     </Wrapper>
   );
